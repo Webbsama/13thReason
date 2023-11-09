45048016879433cdc897ce942c0ebdcbf3480637
coolness10101 <86328220+aarontheo@users.noreply.github.com>
2023-11-09 08:24:16 -0700

    Added -specs for left_min_heap functions, as well as empty function bodies. The squelette now has skin or a liver or sth

--- Week09/heap/src/left_min_heap.erl
+++ Week09/heap/src/left_min_heap.erl
@@ -9,6 +9,32 @@
 get_min(Heap) ->
     todo.
 
+
+-spec insert(term(), heap_node()) -> min_heap().
+insert(Value, Heap) ->
+    todo.
+
+
+-spec merge(min_heap(), min_heap()) -> min_heap().
+merge(A_heap, B_heap) ->
+    todo.
+
+
+-spec build_node(term(), min_heap(), min_heap()) -> min_heap().
+build_node(X, A_heap, B_heap) ->
+    todo.
+
+
+-spec rank(heap_node()) -> integer().
+rank(A_heap) ->
+    todo.
+
+
+-spec remove_min(min_heap()) -> term().
+remove_min(Heap) ->
+    todo.
+
+
 % Test time BBy
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
