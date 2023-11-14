cb440b95e1aa95c16a182842cd8f38f99a5162f3
Webbsama <web18016@byui.edu>
2023-11-14 08:19:40 -0700

    Prepping our tasks erl.

--- Week10/ral/src/ral.erl
+++ Week10/ral/src/ral.erl
@@ -5,14 +5,5 @@
 
 -module(ral).
 
--behaviour(application).
+-export().
 
--export([start/2, stop/1]).
-
-start(_StartType, _StartArgs) ->
-    ral_sup:start_link().
-
-stop(_State) ->
-    ok.
-
-%% internal functions
