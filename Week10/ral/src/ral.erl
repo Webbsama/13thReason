1c8793d8fbba2d467301f780c0cb169c084a50b3
Webbsama <web18016@byui.edu>
2023-11-14 11:57:56 -0700

    Class work on 11/13/23.
    Co-authored-by: aarontheo <aarontheo@users.noreply.github.com>
    Co-authored-by: Kendall Carr <Kendall.Carr@churchofjesuschrist.org>

--- Week10/ral/src/ral.erl
+++ Week10/ral/src/ral.erl
@@ -6,28 +6,33 @@
 -module(ral).
 
 % Need to fix our export once functions have been created. 
--export(todo).
+-export([]).
+
+% Added a type for our ral()
+-type ral() :: []
 
 % Our first main function will be the function called get.
 % Get has three helper functions.
 % 1) search
 % 2) build_bin_list
 % 3) find
--spec get() -> todo.
+% The get function takes two parameters
+-spec get(List, Integer) -> Value.
 
 % Get's first helper function called search
--spec search() -> todo. 
+-spec search() -> todo.
 
 % Get's secound helper function called build_bin_list
 -spec build_bin_list() -> todo.
 
 % Get's thrid helper function called find
--spec find() -> todo. 
+% Find has two parameters the 
+-spec find([]) -> value. 
 
 % Our second main function will be called update
 -spec update() -> todo.
 
-% Our thrid main function will be called replace
+% Our third main function will be called replace
 -spec replace() -> todo. 
 
 % Our fourth main function will be called cons
@@ -39,7 +44,8 @@
 % Will need to add our eunit tests.
 % There will be 5 eunit tests. This is one for each of our main functions
 % Remember that the test's names need to end with an underscore so that the tests compile correctly. 
-
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
 % First test will be for get function
 get_test_() ->
     [
@@ -69,3 +75,5 @@ link_test_() ->
     [
 
     ].
+
+-endif.
