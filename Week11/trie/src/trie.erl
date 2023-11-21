80992cb1fe3393ffcd979a3d4c7e31f3973c3930
coolness10101 <86328220+aarontheo@users.noreply.github.com>
2023-11-21 08:52:46 -0700

    Basic boilerplate for tests, structure comments

---
+++ Week11/trie/src/trie.erl
@@ -0,0 +1,17 @@
+-module(trie).
+
+-export([]).
+
+% Type definitions go here
+
+
+% Function specs and functions go here
+
+
+% Testing segment begins here
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+
+% EUnit function tests go here
+
+-endif.
