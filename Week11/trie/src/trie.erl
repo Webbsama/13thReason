687a81798e965be20d73d3f7a46ea9da2e36d8ce
coolness10101 <86328220+aarontheo@users.noreply.github.com>
2023-11-21 12:37:05 -0700

    Co-authored-by: Kendall Carr <Kendall.Carr@churchofjesuschrist.org> Co-authored-by: Samantha Webb <Sully1810@users.noreply.github.com> Co-authored-by: Jacob Dabling <j-dabling@users.noreply.github.com>

--- Week11/trie/src/trie.erl
+++ Week11/trie/src/trie.erl
@@ -1,17 +1,44 @@
 -module(trie).
 
--export([]).
+-export([lookup/2, add/2]).
 
 %% Type definitions go here
 % trie_node
--type trie_node() :: definition.
+-type trie_node() :: {char(), [trie_node()|nil]}.
 
 %% Function specs and functions go here
 % lookup()
--spec lookup([term()], trie_node()) -> found|found_fail.
-lookup(Target, Trie) ->
-    todo.
+-spec lookup([term()], trie_node()) -> found|not_found.
 
+
+lookup([], Trie) ->
+    {_Value, Children} = Trie,
+    case lists:member(nil, Children) of
+        true ->
+            found;
+        _ ->
+            not_found
+    end;
+lookup(Target, Trie) ->
+    [FirstLetter|Tail] = Target,
+    {_Value, Children} = Trie,
+    Subtree = find_in_list(FirstLetter, Children),
+    case Subtree of
+        not_found ->
+            fail;
+        _ ->
+            lookup(Tail, Subtree)
+        end.
+    
+% If an element in the list matches the patter, returns the element, otherwise return fail.
+find_in_list(_Target, []) -> not_found;
+find_in_list(Target, List = [{Value, _} = Child|T]) ->
+    case Value == Target of
+        true -> 
+            Child;
+        _ -> 
+            find_in_list(Target, T)
+    end.
 % add()
 -spec add([term()], trie_node()) -> trie_node().
 add(Item, Trie) ->
@@ -23,9 +50,18 @@ add(Item, Trie) ->
 
 % EUnit function tests go here
 lookup_test_() ->
-    todo.
+    [
+        % Happy Path
+        ?assertEqual(found, lookup("c", { "c"[Hello, nil]}))
+        ?assertEqual(found, lookup("cat", {"c", [{"a",[{"r", [nil]}, {"t", [nil]}]}]})),
+        ?assertEqual(found, lookup("car", {"c", [{"a",[{"r", [nil]}, {"t", [nil]}]}]})),
+        ?assertEqual(not_found, lookup("dog", {"c", [{"a",[{"r", [nil]}, {"t", [nil]}]}]}))
+    ].
 
 add_test_() ->
-    todo.
+    [
+        % Happy Path
+        ?assertEqual({"c", [{"a",[{"r", [nil]}, {"t", [nil]}]}]}, add("car", {"c", [{"a",[{"t", [nil]}]}]}))
+    ].
 
 -endif.
