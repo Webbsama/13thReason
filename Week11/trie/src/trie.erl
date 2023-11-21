3e5fe2bfee7ff0b5bb22f730e384af9cc2404674
coolness10101 <86328220+aarontheo@users.noreply.github.com>
2023-11-21 08:59:35 -0700

    Added function bodies and specs

--- Week11/trie/src/trie.erl
+++ Week11/trie/src/trie.erl
@@ -4,13 +4,18 @@
 
 %% Type definitions go here
 % trie_node
-
+-type trie_node() :: definition.
 
 %% Function specs and functions go here
 % lookup()
+-spec lookup([term()], trie_node()) -> found|found_fail.
+lookup(Target, Trie) ->
+    todo.
 
 % add()
-
+-spec add([term()], trie_node()) -> trie_node().
+add(Item, Trie) ->
+    todo.
 
 %% Testing segment begins here
 -ifdef(TEST).
