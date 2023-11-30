2eb11e56dd2052193f032a7f5b33c64b9f242c50
coolness10101 <86328220+aarontheo@users.noreply.github.com>
2023-11-30 12:37:48 -0700

    We 'trie'd.

--- Week11/trie/src/trie.erl
+++ Week11/trie/src/trie.erl
@@ -10,7 +10,6 @@
 % lookup()
 -spec lookup([term()], trie_node()) -> found|not_found.
 
-
 lookup([], Trie) ->
     {_Value, Children} = Trie,
     case lists:member(nil, Children) of
@@ -22,24 +21,24 @@ lookup([], Trie) ->
 lookup(Target, Trie) ->
     [FirstLetter|Tail] = Target,
     {_Value, Children} = Trie,
-    Subtrie = search_children(FirstLetter, Children),
+    Subtrie = retrieve_children(FirstLetter, Children),
     case Subtrie of
         not_found ->
             not_found;
         _ ->
             lookup(Tail, Subtrie)
         end.
-    
-% If an element in the list matches the patter, returns the element, otherwise return fail.
--spec search_children(term(), [trie_node()|nil]) -> trie_node()|nil.
-search_children(_Target, []) -> not_found;
-search_children(Target, [nil|T]) -> search_children(Target, T);
-search_children(Target, [{Value, Children}|T]) ->
+
+% If an element in the list matches the pattern, returns the element, otherwise return fail.
+-spec retrieve_children(term(), [trie_node()|nil]) -> trie_node()|fail.
+retrieve_children(_Target, []) -> fail;
+retrieve_children(Target, [nil|T]) -> retrieve_children(Target, T);
+retrieve_children(Target, [{Value, Children}|T]) ->
     case Value == Target of
         true -> 
             {Value, Children};
         _ -> 
-            search_children(Target, T)
+            retrieve_children(Target, T)
     end.
 
 % add()
@@ -52,19 +51,29 @@ add(Item, Trie) ->
 -include_lib("eunit/include/eunit.hrl").
 
 % EUnit function tests go here
+retrieve_children_test_() ->
+    [
+        % Happy Path
+        ?assertEqual([{a,[{r, [nil]}, {t, [nil]}]}], retrieve_children(t, 
+        [
+            {a,[{r, [nil]}, 
+            {t, [nil]}]}
+        ]))
+    ].
+
 lookup_test_() ->
     [
         % Happy Path
-        ?assertEqual(found, lookup("c", { "c", [nil]})),
-        ?assertEqual(found, lookup("cat", {"c", [{"a",[{"r", [nil]}, {"t", [nil]}]}]})),
-        ?assertEqual(found, lookup("car", {"c", [{"a",[{"r", [nil]}, {"t", [nil]}]}]})),
-        ?assertEqual(not_found, lookup("dog", {"c", [{"a",[{"r", [nil]}, {"t", [nil]}]}]}))
+        ?assertEqual(found, lookup([c], { c, [nil]})),
+        ?assertEqual(found, lookup([c, a, t], {c, [{a,[{r, [nil]}, {t, [nil]}]}]})),
+        ?assertEqual(found, lookup([c, a, r], {c, [{a,[{r, [nil]}, {t, [nil]}]}]})),
+        ?assertEqual(not_found, lookup([d, o, g], {c, [{a,[{r, [nil]}, {t, [nil]}]}]}))
     ].
 
-add_test_() ->
-    [
-        % Happy Path
-        ?assertEqual({"c", [{"a",[{"r", [nil]}, {"t", [nil]}]}]}, add("car", {"c", [{"a",[{"t", [nil]}]}]}))
-    ].
+% add_test_() ->
+%     [
+%         % Happy Path
+%         ?assertEqual({"c", [{"a",[{"r", [nil]}, {"t", [nil]}]}]}, add("car", {"c", [{"a",[{"t", [nil]}]}]}))
+%     ].
 
 -endif.
